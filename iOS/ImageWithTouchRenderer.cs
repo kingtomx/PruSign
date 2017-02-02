2141ec4347a9c9f2cb8e0566233e5dd531f9ca7e
TOMAS PASCUAL GORDO <tomaspascualgordo@TOMASs-MacBook-Pro.local>
2017-02-02 14:57:03 -0300

    commit

---
+++ iOS/ImageWithTouchRenderer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace PruSign.iOS
+{
+	public class ImageWithTouchRenderer : ContentPage
+	{
+		public ImageWithTouchRenderer()
+		{
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
