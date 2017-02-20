e6bd9e8d5afdeb907d2ed449d1acef020d4f5bd3
kingtomx <tomas.pascual.home@gmail.com>
2017-02-20 16:46:32 -0300

    commit

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -10,6 +10,8 @@ namespace PruSign.iOS
 	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
 	{
 
+		public NSUrlSessionUploadTask uploadTask;
+
 		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
 		{
 			global::Xamarin.Forms.Forms.Init();
@@ -19,11 +21,38 @@ namespace PruSign.iOS
 
 		public override void DidEnterBackground(UIApplication app)
 		{
+			UploadData("https://reqres.in/api/users");
+		}
+
+
+		public void UploadData(string url)
+		{
 			NSUrlSession session = null;
 
 			NSUrlSessionConfiguration configuration = NSUrlSessionConfiguration.CreateBackgroundSessionConfiguration("com.SimpleBackgroundTransfer.BackgroundSession");
 			session = NSUrlSession.FromConfiguration(configuration, (NSUrlSessionDelegate)new MySessionDelegate(), new NSOperationQueue());
-		}
+
+			// URL
+			NSMutableUrlRequest request = new NSMutableUrlRequest(new NSUrl(url));
+			// METHOD
+			request.HttpMethod = "POST";
+			// HEADERS
+			request.HttpMethod = "POST";
+			var keys = new object[] { "Key1", "Key2" };
+			var objects = new object[] { "Value1", "Value2" };
+			var dictionnary = NSDictionary.FromObjectsAndKeys(objects, keys);
+			request.Headers = dictionnary;
+
+			NSObject postObj = FromObject("yourfieldname=yourpassedvalue");
+			NSString postString = (NSString)postObj;
+			NSData postData = NSData.FromString(postString);
+
+			request.Body = postData;
+
+			uploadTask = session.CreateUploadTask(request);
+			uploadTask.Resume();
+
+			}
 
 
 	}
