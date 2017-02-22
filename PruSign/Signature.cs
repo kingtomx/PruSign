65b3fea77064ccf562d54e0fc8ad98e1c292f1d8
kingtomx <tomas.pascual.home@gmail.com>
2017-02-22 12:18:44 -0300

    commit

--- PruSign/Signature.cs
+++ PruSign/Signature.cs
@@ -4,11 +4,13 @@ namespace PruSign
 	public class Signature
 	{
 		public byte[] image;
+		public PruSign.iOS.PointWhen[] points;
 		public String datetime;
 		public String customerName;
 		public String customerId;
 		public String documentId;
 		public String applicationId;
 		public String hash;
+
 	}
 }
