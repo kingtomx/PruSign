bb06b715cec8cfd5db6f320876b14521cb36c984
kingtomx <tomas.pascual.home@gmail.com>
2017-05-10 14:40:08 -0300

    commit

--- Droid/DrawView.cs
+++ Droid/DrawView.cs
@@ -1,6 +1,8 @@
 ﻿using Android.Views;
 using Android.Graphics;
 using Android.Content;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace PruSign.Android
 {
@@ -18,12 +20,15 @@ namespace PruSign.Android
 		private Paint CanvasPaint;
 		private Canvas DrawCanvas;
 		private Bitmap CanvasBitmap;
+		public List<Droid.PointWhen> points;
+
 
 
 		private void Start()
 		{
 			CurrentLineColor = Color.Black;
 			PenWidth = 3.0f;
+			points = new List<Droid.PointWhen>();
 
 			DrawPath = new Path();
 			DrawPaint = new Paint
@@ -87,6 +92,22 @@ namespace PruSign.Android
 				case MotionEventActions.Up:
 					DrawCanvas.DrawPath(DrawPath, DrawPaint);
 					DrawPath.Reset();
+					//Aqui podemos crear una carpeta para firmas temporales
+					var documents = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+					var directoryname = System.IO.Path.Combine(documents, "temporalSignatures");
+					System.IO.Directory.CreateDirectory(directoryname);
+
+					byte[] imageByteArray = CapturePNG(this);
+					long ticks = System.DateTime.Now.Ticks;
+					string pngFilename = System.IO.Path.Combine(directoryname, "signature.png");
+					System.IO.File.WriteAllBytes(pngFilename, imageByteArray);
+
+
+					// Salvamos tambien el fichero de puntos
+					string pointsFilename = System.IO.Path.Combine(directoryname, "points.json");
+					string pointsString = JsonConvert.SerializeObject(points);
+					System.IO.File.WriteAllText(pointsFilename, pointsString);
+
 					break;
 				default:
 					return false;
@@ -96,5 +117,24 @@ namespace PruSign.Android
 
 			return true;
 		}
+
+
+
+		public byte[] CapturePNG(View view, bool autoScale = true)
+		{
+			var wasDrawingCacheEnabled = view.DrawingCacheEnabled;
+			view.DrawingCacheEnabled = false;
+			view.BuildDrawingCache(autoScale);
+			var bitmap = view.GetDrawingCache(autoScale);
+			view.DrawingCacheEnabled = wasDrawingCacheEnabled;
+			byte[] bitmapData;
+			using (var stream = new System.IO.MemoryStream())
+			{
+			    bitmap.Compress(Bitmap.CompressFormat.Png, 0, stream);
+			    bitmapData = stream.ToArray();
+			}
+			return bitmapData;
+		}
+
 	}
 }
--- Droid/PointWhen.cs
+++ Droid/PointWhen.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Drawing;
+using Android.Graphics;
 
 namespace PruSign.Droid
 {
