19e01b343026dc17d34ab338555df02166d7e35b
kingtomx <tomas.pascual.home@gmail.com>
2017-05-15 10:38:02 -0300

    commit

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -7,6 +7,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.Threading;
 
 namespace PruSign.Droid
 {
@@ -24,6 +25,43 @@ namespace PruSign.Droid
 
 			LoadApplication(new App());
 		}
+
+
+
+		protected override void OnStop()
+		{
+			base.OnStop();
+
+			ThreadPool.QueueUserWorkItem (o => sendRestSignature ());
+
+		}
+
+		private void sendRestSignature()
+		{
+			try
+			{
+				FileHelper fh = new FileHelper();
+				SignatureDatabase db = new SignatureDatabase(fh.GetLocalFilePath("PruSign.db"));
+				System.Threading.Tasks.Task<System.Collections.Generic.List<SignatureItem>> items = db.GetItemsNotDoneAsync();
+				foreach (var item in items.Result)
+				{
+					try
+					{
+						//Post("https://48.177.151.81:29398/api/SignatureApi", item.SignatureObject);
+						item.Sent = true;
+						item.SentTimeStamp = System.DateTime.Now.Ticks;
+						db.SaveItemAsync(item);
+					} catch (Exception ex) {
+						// POR AHORA HAGAMOS ESTO EN CASO DE ERROR
+						item.Miscelanea = ex.Message;
+						db.SaveItemAsync(item);
+					}
+				}
+			} catch (Exception ex) {
+				String error = ex.Message;
+			}
+		}
+
 
 
 	}
