e7b92e952df16f28f89febc4ffe59d4e280ce925
kingtomx <tomas.pascual.home@gmail.com>
2017-05-16 13:55:14 -0300

    commit

--- PruSignBackEnd/Controllers/SignatureApiController.cs
+++ PruSignBackEnd/Controllers/SignatureApiController.cs
@@ -1,10 +1,39 @@
 ﻿using System;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+
 namespace PruSignBackEnd
 {
-	public class SignatureApiController
+	public class SignatureApiController : ApiController
 	{
-		public SignatureApiController()
+
+
+		public string get(int id)
 		{
+			return "value";
 		}
+
+		public HttpResponseMessage Post(PruSignBackEnd.PruSign.Object.Signature signature)
+		{
+
+			try
+			{
+
+				string appId = signature.applicationId;
+
+				return new HttpResponseMessage(HttpStatusCode.OK);
+
+			}
+			catch (Exception ex)
+			{
+				return new HttpResponseMessage(HttpStatusCode.OK);
+			}
+
+
+		}
+
+
+
 	}
 }
